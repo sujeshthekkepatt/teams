56757734ce3a710fa11a58c1d6df78fc03a98156
sujesh <sujeshthekkepatt@gmail.com>
2021-02-23 14:23:39 +0530

    removing types package

--- client/client.go
+++ client/client.go
@@ -6,9 +6,29 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-
-	"github.com/sujeshthekkepatt/teams/types"
 )
+
+//Facts -struct
+type Facts struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+//Sections -struct
+type Sections struct {
+	ActivityTitle    string  `json:"activityTitle"`
+	ActivitySubtitle string  `json:"activitySubtitle"`
+	Facts            []Facts `json:"facts"`
+	Markdown         bool    `json:"markdown"`
+}
+
+//Payload -struct
+type Payload struct {
+	Type       string     `json:"@type"`
+	ThemeColor string     `json:"themeColor"`
+	Summary    string     `json:"summary"`
+	Sections   []Sections `json:"sections"`
+}
 
 func createClient() http.Client {
 
@@ -47,7 +67,7 @@ func TransportEvent(teamsPayload []byte, teamsURL string) (*http.Response, error
 }
 
 //MarshalMessage marshal message to json
-func MarshalMessage(message types.Payload) (teamMessage []byte, err error) {
+func MarshalMessage(message Payload) (teamMessage []byte, err error) {
 
 	teamMessage, err = json.Marshal(message)
 
